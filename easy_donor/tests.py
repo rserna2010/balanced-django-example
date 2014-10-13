71e054aacb66091e3a876bc001a68f8251adccb9
richie serna <richie@balancedpayments.com>
2014-10-13 11:55:35 -0700

    Add tests for checking phone number formating

--- easy_donor/tests.py
+++ easy_donor/tests.py
@@ -2,8 +2,6 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
-
-
 from easy_donor.models import Charity, Donation
 
 import balanced
@@ -32,6 +30,7 @@ FIXTURES = {
         'url': 'http://www.salvationarmyusa.org'
     }
 }
+
 
 def create_charity():
     charity = Charity(business_name="Salvation Army",
@@ -101,6 +100,7 @@ class CharityModelTests(TestCase):
         with self.assertRaises(ValueError):
             charity.save()
 
+
 class CharityViewTests(TestCase):
     def test_index_view_with_no_charities(self):
         response = self.client.get(reverse('easy_donor:index'))
@@ -116,17 +116,9 @@ class CharityViewTests(TestCase):
         self.assertQuerysetEqual(response.context['charity_list'],
                                  ['<Charity: Salvation Army>'])
 
-    def test_charity_page_returns_appropriate_info(self):
-        charity = create_charity()
-        response = self.client.get(reverse('easy_donor:charity', args=[1]))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, charity.business_name)
-        self.assertContains(response, charity.url)
-        self.assertContains(response, charity.description)
-
 class CharityFormTests(TestCase):
     def test_create_charity_form(self):
-        response = self.client.post(reverse('easy_donor:index'),
+        response = self.client.post(reverse('easy_donor:sign_up'),
                                     {'business_name': 'Salvation Army',
                                      'ein': '123456789',
                                      'email': 'sal@salvationarmy.com',
@@ -135,13 +127,14 @@ class CharityFormTests(TestCase):
                                                     'charitable organization.',
                                      'url': 'http://www.salvationarmyusa.org'})
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['charity_id'], 1)
 
     def test_charity_form_with_short_ein(self):
         response = self.client.post(reverse('easy_donor:sign_up'),
                                     {'business_name': 'Salvation Army',
                                      'ein': '12345678',
                                      'email': 'sal@salvationarmy.com',
-                                     'phone':'7131234567',
+                                     'phone': '7131234567',
                                      'description': 'An international '
                                                     'charitable organization.',
                                      'url': 'http://www.salvationarmyusa.org'})
@@ -153,9 +146,34 @@ class CharityFormTests(TestCase):
                                         {'business_name': 'Salvation Army',
                                          'ein': '12345678A',
                                          'email': 'sal@salvationarmy.com',
-                                         'phone':'7131234567',
+                                         'phone': '7131234567',
                                          'description': 'An international '
                                                         'charitable organization.',
                                          'url': 'http://www.salvationarmyusa.org'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "value must be an integer")
+
+    def test_charity_form_with_invalid_phone_number(self):
+        response = self.client.post(reverse('easy_donor:sign_up'),
+                                        {'business_name': 'Salvation Army',
+                                         'ein': '123456789',
+                                         'email': 'sal@salvationarmy.com',
+                                         'phone': '123b',
+                                         'description': 'An international '
+                                                        'charitable organization.',
+                                         'url': 'http://www.salvationarmyusa.org'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Phone number must be entered in the"
+                                      " format")
+
+    def test_charity_form_with_invalid_phone_number(self):
+        response = self.client.post(reverse('easy_donor:sign_up'),
+                                    {'business_name': 'Salvation Army',
+                                     'ein': '123456789',
+                                     'email': 'sal@salvationarmy.com',
+                                     'phone': '7131234567',
+                                     'description': 'An international '
+                                                    'charitable organization.',
+                                     'url': 'salvationarmyusa'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Please enter a valid URL")
--- easy_donor/views.py
+++ easy_donor/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 from easy_donor.models import Charity, Donation
 
+import re
 import balanced
 balanced.configure(settings.BALANCED['secret'])
 
@@ -117,6 +118,19 @@ def donate(request):
 
 class CharityForm(ModelForm):
     ein = forms.CharField(max_length=9, min_length=9)
+    phone = forms.RegexField(regex=r'^\+?1?\d{9,12}$',
+                             error_message=
+                             ("Phone number must be entered in the format: "
+                              "'+999999999'. Up to 12 digits allowed."))
+    url = forms.RegexField(
+        regex=re.compile(
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE),
+        error_message=("Please enter a valid URL")
+    )
 
     class Meta:
         model = Charity
